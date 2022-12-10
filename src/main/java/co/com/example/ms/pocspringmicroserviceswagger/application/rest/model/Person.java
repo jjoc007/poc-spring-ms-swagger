82bb11d2926fcd9004ae645adc53297e0bbea79b
Juan Orjuela <juanjose.orjuela@mercadolibre.com.co>
2022-12-10 11:48:41 -0500

    ajuste person

--- src/main/java/co/com/example/ms/pocspringmicroserviceswagger/application/rest/model/Person.java
+++ src/main/java/co/com/example/ms/pocspringmicroserviceswagger/application/rest/model/Person.java
@@ -1,18 +1,45 @@
 package co.com.example.ms.pocspringmicroserviceswagger.application.rest.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
+@Schema(description = "entidad persona")
 @Getter
+@Setter
 public class Person {
+
+	@Schema(
+			description = "Id de la persona",
+			example = "123456789"
+	)
 	private Long id;
 
+	@Schema(
+			description = "Nombre de la persona",
+			example = "pepito",
+			required = true
+	)
 	private String name;
 
+	@Schema(
+			description = "Apellido de la persona",
+			example = "perez",
+			required = true
+	)
 	private String lastName;
 
+	@Schema(
+			description = "telefono de la persona",
+			example = "573126669997",
+			required = true
+	)
 	private String phone;
 
+	@Schema(
+			description = "Direccion de la persona",
+			example = "calle falsa 123",
+			required = true
+	)
 	private String address;
 }
